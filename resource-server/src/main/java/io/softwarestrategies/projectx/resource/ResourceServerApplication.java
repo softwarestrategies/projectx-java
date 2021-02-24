7269d64b2f301130dc8daa0d7f35fe7ec0069423
henrydall@softwarestrategies.io <henrydall@softwarestrategies.io>
2021-02-24 11:17:36 -0800

    It was running Tomcat instead of Netty, which was suboptimal.  These changes fix that.

--- resource-server/src/main/java/io/softwarestrategies/projectx/resource/ResourceServerApplication.java
+++ resource-server/src/main/java/io/softwarestrategies/projectx/resource/ResourceServerApplication.java
@@ -1,13 +1,16 @@
 package io.softwarestrategies.projectx.resource;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ResourceServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ResourceServerApplication.class, args);
+        SpringApplication app = new SpringApplication(ResourceServerApplication.class);
+        app.setWebApplicationType(WebApplicationType.REACTIVE);
+        app.run(args);
     }
 
 }
